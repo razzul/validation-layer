dcbc60707a9000de96f73c98651e88d6079285fc
Razzul <developer.rajul@gmail.com>
2017-08-31 18:00:12 +0530

    update

--- configs/api_config.php
+++ configs/api_config.php
@@ -2,32 +2,68 @@
 
 global $api_config;
 
+/**
+ * --------------------------------------
+ * LAYERS
+ * --------------------------------------
+ *
+ * pre         => PreValidationLayer
+ * bussiness   => BussinessValidationLayer
+ * pms         => PMSValidationLayer
+ * before_save => BeforeSaveValidationLayer
+ * -----------------------------------------
+ */
+
+/**
+ * ------------------------------------------------------------
+ * EXAMPLE
+ * ------------------------------------------------------------
+ * 
+ * // priority
+ * $api_config['xpath'][] = [
+ *     // group
+ *     [
+ *         'field_name' => [
+ *             'type'    => 'invalid|mandatory',
+ *             'layer'   => 'pre|bussiness|pms|before_save',
+ *             'code'    => 'error code',
+ *             'message' => 'error message',
+ *             'details' => 'error details with xpath and {xpath_ids}"',
+ *         ],
+ *     ],
+ * ];
+ * ------------------------------------------------------------
+ *
+ * Once added we need to added a function to the respective 'layer'
+ * and method name should be [pre|bussiness|pms|before_save]_[invalid|mandatory]_[field_name]
+ */
+
 // priority 1
 $api_config['xpath'][] = [
-	// group A
-	[
-		'http_method' => [
-			'type'     => 'invalid',
-			'layer'    => 'pre',
-			'code'     => '4050101001',
-			'message'  => 'Invalid HTTP Method.',
-			'details'  => 'HTTP method "{http_method}" is invalid for "api/v2/contract-information"',
-		],
-	],
+    // group A
+    [
+        'http_method' => [
+            'type'    => 'invalid',
+            'layer'   => 'pre',
+            'code'    => '4050101001',
+            'message' => 'Invalid HTTP Method.',
+            'details' => 'HTTP method "{http_method}" is invalid for "api/v2/contract-information"',
+        ],
+    ],
 ];
 
 // priority 2
 $api_config['xpath'][] = [
-	// group B
-	[
-		'session' => [
-			'type'     => 'invalid',
-			'layer'    => 'pre',
-			'code'     => '4000201001',
-			'message'  => 'Invalid Session Id.',
-			'details'  => '',
-		]
-	]
+    // group B
+    [
+        'session' => [
+            'type'    => 'invalid',
+            'layer'   => 'pre',
+            'code'    => '4000201001',
+            'message' => 'Invalid Session Id.',
+            'details' => '',
+        ],
+    ],
 ];
 
 // priority 3
@@ -35,26 +71,26 @@ $api_config['xpath'][] = [];
 
 // priority 4
 $api_config['xpath'][] = [
-	// group A
-	[
-		'contract_id' => [
-			'type'     => 'mandatory',
-			'layer'    => 'bussiness',
-			'code'     => '4050101001',
-			'message'  => 'Contract process terminated. Contract Reference ID (GCM ID or Contract UUID) should not be empty.',
-			'details'  => '"bi:contract-id" prarameter is mandatory.',
-		]
-	],
-	// group B
-	[
-		'contract_name' => [
-			'type'     => 'mandatory',
-			'layer'    => 'bussiness',
-			'code'     => '4000201001',
-			'message'  => 'Contract process terminated. Mandatory element(s) should not be empty.',
-			'details'  => '/contract-info:contract-information/bi:product-contracts/bi:product-contract [@id="{contract_name}:1"]/cbe:name is mandatory.',
-		]
-	]
+    // group A
+    [
+        'contract_id' => [
+            'type'    => 'mandatory',
+            'layer'   => 'bussiness',
+            'code'    => '4050101001',
+            'message' => 'Contract process terminated. Contract Reference ID (GCM ID or Contract UUID) should not be empty.',
+            'details' => '"bi:contract-id" prarameter is mandatory.',
+        ],
+    ],
+    // group B
+    [
+        'contract_name' => [
+            'type'    => 'mandatory',
+            'layer'   => 'bussiness',
+            'code'    => '4000201001',
+            'message' => 'Contract process terminated. Mandatory element(s) should not be empty.',
+            'details' => '/contract-info:contract-information/bi:product-contracts/bi:product-contract [@id="{contract_name}:1"]/cbe:name is mandatory.',
+        ],
+    ],
 ];
 
 // priority 5
@@ -71,22 +107,22 @@ $api_config['xpath'][] = [];
 
 // priority 9
 $api_config['xpath'][] = [
-	// group B
-	[
-		'characteristic_id' => [
-			'type'     => 'mandatory',
-			'layer'    => 'pms',
-			'code'     => '4000201001',
-			'message'  => 'Contract process terminated. Mandatory element(s) should not be empty.',
-			'details'  => '/contract-info:contract-information/bi:product-biitems/bi:product-biitem [@id="{characteristic_id}:1"] characteristic is mandatory.',
-		],
+    // group B
+    [
+        'characteristic_id' => [
+            'type'    => 'mandatory',
+            'layer'   => 'pms',
+            'code'    => '4000201001',
+            'message' => 'Contract process terminated. Mandatory element(s) should not be empty.',
+            'details' => '/contract-info:contract-information/bi:product-biitems/bi:product-biitem [@id="{characteristic_id}:1"] characteristic is mandatory.',
+        ],
 
-		'bi_action' => [
-			'type'     => 'mandatory',
-			'layer'    => 'bussiness',
-			'code'     => '4000201001',
-			'message'  => 'Contract process terminated. Mandatory element(s) should not be empty.',
-			'details'  => 'Biitem "bi:action" XPATH with [@id/temporary-id="{bi_action}"]+ is mandatory.',
-		]
-	]
+        'bi_action'         => [
+            'type'    => 'mandatory',
+            'layer'   => 'bussiness',
+            'code'    => '4000201001',
+            'message' => 'Contract process terminated. Mandatory element(s) should not be empty.',
+            'details' => 'Biitem "bi:action" XPATH with [@id/temporary-id="{bi_action}"]+ is mandatory.',
+        ],
+    ],
 ];
